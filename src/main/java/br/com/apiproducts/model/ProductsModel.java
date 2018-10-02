8c9bb9a168e0133dfcd074fc0315e6578d46aa3f
Michel Spirlandeli <michelspirlandeli10@gmail.com>
2018-10-02 19:42:04 -0300

    add lombok and products model

--- src/main/java/br/com/apiproducts/model/ProductsModel.java
+++ src/main/java/br/com/apiproducts/model/ProductsModel.java
@@ -1,4 +1,23 @@
 package br.com.apiproducts.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name="TB_PRODUCTS")
 public class ProductsModel {
+
+    @Id
+    @GeneratedValue(strategy= GenerationType.AUTO)
+    private long id;
+    private String nome;
+    private BigDecimal quantidade;
+    private BigDecimal valor;
 }
