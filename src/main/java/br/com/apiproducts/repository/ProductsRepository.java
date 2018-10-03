a67914839a8eeb17e8a84cadeff9e70cd71411da
Michel Spirlandeli <michelspirlandeli10@gmail.com>
2018-10-03 16:48:09 -0300

    add repository produtcs

--- src/main/java/br/com/apiproducts/repository/ProductsRepository.java
+++ src/main/java/br/com/apiproducts/repository/ProductsRepository.java
@@ -1,4 +1,11 @@
 package br.com.apiproducts.repository;
 
-public class ProductsRepository {
+import br.com.apiproducts.model.ProductsModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductsRepository extends JpaRepository<ProductsModel, Long> {
+
+    ProductsModel findById(long id);
 }
